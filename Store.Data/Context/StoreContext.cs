21d9e7b4e7d5521d66391da9321c14d5c8267ebd
amir hossein mahmoodpour <amirhossein5starstar@outlook.com>
2021-09-30 13:54:56 +0330

    کاربر و مقام کاربر روابتش تموم شد

---
+++ Store.Data/Context/StoreContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities.User;
+
+namespace Store.Data.Context
+{
+   public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        #region User
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+}
--- Store/Startup.cs
+++ Store/Startup.cs
@@ -7,16 +7,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Store.Data.Context;
 
 namespace Store
 {
     public class Startup
     {
-        
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
+            #region DataBase Context
+
+            services.AddDbContext<StoreContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("StoreConnection"));
+                }
+            );
+            #endregion
 
         }
 
